257948ef6719065a475fd6429ef0dd7a96d009da
Marcos <marcosbohorquez0910@gmail.com>
2026-08-01 10:16:52 -0500

    se creo el repositorio para el backend del sistema de vacunacion

---
+++ Backend/Delta/src/main/java/com/sistema_de_vacunacion/Delta/DeltaApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_de_vacunacion.Delta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaApplication.class, args);
+	}
+
+}
---
+++ Backend/Delta/src/test/java/com/sistema_de_vacunacion/Delta/DeltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_de_vacunacion.Delta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
